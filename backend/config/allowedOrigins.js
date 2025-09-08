9d0acf56c3d36426f44813b389edcc3658398e6f
shreejavasala <shreevasala29@gmail.com>
2025-09-08 06:14:08 +0530

    Update allowedOrigins.js

--- backend/config/allowedOrigins.js
+++ backend/config/allowedOrigins.js
@@ -1,3 +1,7 @@
 export const allowedOrigins = [
-  "https://my-shopify-blond.vercel.app"
+  "https://my-shopify-blond.vercel.app",
+  "https://vercel.com/shreejavasalas-projects/my-shopify/A8nwUfnEcEDMzKqBDUbsd9VkwJUC",
+  "https://my-shopify-clvx.vercel.app",
+  "http://localhost:5173",               
+  "http://localhost:3000"
 ]
